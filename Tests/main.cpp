41f7b0150361ff756c31eb0b836c4290888e3253
GAP-team-git <giuseppe.coppini@gmail.com>
2026-03-06 15:44:44 +0100

    Google tests (slice error)

---
+++ Tests/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tests
+//
+//  Created by Giuseppe Coppini on 05/03/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
