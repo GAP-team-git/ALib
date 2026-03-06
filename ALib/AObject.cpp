47176808a9ee6db37c30c1c33f91cdcf0dd2d06b
GAP-team-git <giuseppe.coppini@gmail.com>
2026-03-06 17:49:41 +0100

    Versione con super test OK

--- ALib/AObject.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  GObject.cpp
-//  ALib
-//
-//  Created by Giuseppe Coppini on 25/02/26.
-//
-
-#include <stdio.h>
---
+++ MultipleTests/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MultipleTests
+//
+//  Created by Giuseppe Coppini on 06/03/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
